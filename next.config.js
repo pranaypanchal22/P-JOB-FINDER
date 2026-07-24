1728cbc2199cbab11083194a1cd4d8ec3188e2ff
Pranay <panchalpranay22@gmail.com>
2026-07-24 11:20:09 -0700

    Simplify next.config, rely on tsconfig path resolution

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src/'),
-      '@/components': path.resolve(__dirname, 'src/components/'),
-      '@/lib': path.resolve(__dirname, 'src/lib/'),
-      '@/server': path.resolve(__dirname, 'src/server/'),
-      '@/types': path.resolve(__dirname, 'src/types/'),
-    }
-    return config
+  typescript: {
+    tsconfigPath: './tsconfig.json',
   },
 }
 
